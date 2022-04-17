d318de11c1fb1dcb1144b944723c995cf1e2ab94
Eduardo Lacerda <eduardolacerda@edu.unifil.br>
2022-04-17 20:54:51 -0300

    :sparkles: Adicionada tratavida no banco de dados

--- database/migrations/2022_04_14_232359_chance_collomn.php
+++ database/migrations/2022_04_14_232359_chance_collomn.php
@@ -13,7 +13,55 @@ class ChanceCollomn extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('pets', function (Blueprint $table) {
+            $affected = DB::table('pets')
+                ->where('specie','bunny')
+                ->update(
+                    ['specie'=>'coelho']
+                );
+            $affected = DB::table('pets')
+                ->whereIn('specie',['mamba','mamba-negra'])
+                ->update(
+                    ['specie'=>'cobra']
+                );
+            $affected = DB::table('pets')
+                ->whereIn('specie',['pikachu','bulbasauro','squirtle'])
+                ->update(
+                    ['specie'=>'pokemon']
+                );
+            $affected = DB::table('pets')
+                ->whereIn('specie',['dragao de komodo','dragão de komodo'])
+                ->update(
+                    ['specie'=>'dragao-de-komodo']
+                );
+            $affected = DB::table('pets')
+                ->whereIn('specie',['papagaio','periquito'])
+                ->update(
+                    ['specie'=>'ave']
+                );
+            $affected = DB::table('pets')
+                ->where('specie','dog')
+                ->update(
+                    ['specie'=>'cachorro']
+                );
+            
+            $affected = DB::table('pets')
+                ->whereIn('size',['SM','small'])
+                ->update(
+                    ['size'=>'sm']
+                );
+            $affected = DB::table('pets')
+                ->whereIn('size',['large','L'])
+                ->update(
+                    ['size'=>'l']
+                );
+            $affected = DB::table('pets')
+                ->whereIn('size',['medium','M'])
+                ->update(
+                    ['size'=>'m']
+                );
+        }); 
+
     }
 
     /**
